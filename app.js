dea103146edf4f11866d7379c9ac1531db00d14f
Vinicius <vini2011almeida@gmail.com>
2022-06-14 22:12:10 -0300

    main

---
+++ app.js
@@ -0,0 +1,18 @@
+const { response } = require('express');
+var express = require('express');
+var {engine} = require('express-handlebars');
+
+
+var app = express();
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.get('/', function( request, response){
+    response.render('index');
+});
+
+app.use(express.static('public'));
+app.listen(3000);
+
