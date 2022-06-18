b56b36e886bc720abe1b2d201178296ab8a3c1f4
BrunoBarlaan <brunobarlaan@gmai.com>
2022-06-18 19:50:36 -0300

    cadastro servico/app.js

--- app.js
+++ app.js
@@ -1,30 +1,44 @@
 const { response } = require('express');
 var express = require('express');
 var {engine} = require('express-handlebars');
-
+//var bp = require('body-parser');
 
 var app = express();
+var servicos = [];
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
-app.get('/', function( request, response){
-    response.render('index');
+//app.use(bp.urlencoded({extend: false}))
+//app.use(bp.json());
+
+app.get('/', function( req, res){
+    res.render('index');
 });
 
-app.get('/cadastroservico', function( request, response){
-    response.render('cadastroServico');
+app.get('/cadastroservico', function( req, res){
+    res.render('cadastroServico');
 });
 
-app.get('/prestadores', function( request, response){
-    response.render('prestadores');
+app.get('/prestadores', function( req, res){
+    res.render('prestadores');
 });
 
-app.get('/servicos', function( request, response){
-    response.render('servicos');
+app.get('/servicos', function( req, res){
+    res.render('servicos', {servicos});
 });
+
+app.post('/cadastroServico', function(req,res){
+    localidade = req.body.localidade;
+    nomeCategoria = req.body.nomeCategoria;
+    nomeServiço = req.body.nomeServiço;
+    servico = {"localidade": localidade,
+                 "nomeCategoria": nomeCategoria,
+                 "nomeServico": nomeServico};
+    servicos.push(servico);
+    res.redirect('/servicos', {servicos});
+})
 
 app.use(express.static('public'));
 app.listen(3000);
-
